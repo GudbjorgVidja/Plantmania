9c5c308a266c8ccfa3b1e67eb22ea7abcb385a9b
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-02 14:21:09 +0000

    viðbætur við stylesheet

--- src/main/java/vidmot/plantmania/PlantApplication.java
+++ src/main/java/vidmot/plantmania/PlantApplication.java
@@ -12,7 +12,7 @@ public class PlantApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PlantApplication.class.getResource("mania-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Plants in pants!");
         stage.setScene(scene);
         stage.show();
     }
