5cbf450fac8d4d2a3b53f3615edaf0ad09f8da39
Sigurbjörg Erla <sepbiering@gmail.com>
2023-04-02 19:35:02 +0000

    bætti við Notandi vinnsluklasa

---
+++ src/main/java/vinnsla/plantmania/Notandi.java
@@ -0,0 +1,48 @@
+package vinnsla.plantmania;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Notandi {
+    private StringProperty notendanafn = new SimpleStringProperty();
+    private StringProperty lykilord = new SimpleStringProperty();
+    
+    public Notandi(String notendanafn, String lykilord) {
+        this.notendanafn.set(notendanafn);
+        this.lykilord.set(lykilord);
+    }
+
+    public Notandi() {
+    }
+
+    public String getNotendanafn() {
+        return notendanafn.get();
+    }
+
+    public StringProperty notendanafnProperty() {
+        return notendanafn;
+    }
+
+    public void setNotendanafn(String notendanafn) {
+        this.notendanafn.set(notendanafn);
+    }
+
+    public String getLykilord() {
+        return lykilord.get();
+    }
+
+    public StringProperty lykilordProperty() {
+        return lykilord;
+    }
+
+    public void setLykilord(String lykilord) {
+        this.lykilord.set(lykilord);
+    }
+
+    public String toString() {
+        return "Notandi{" +
+                "notendanafn=" + notendanafn.get() +
+                ", lykilord=" + lykilord.get() +
+                '}';
+    }
+}
