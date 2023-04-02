512046628a26e6d1b8ce45d623b4fc065d10b689
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-02 22:00:26 +0000

    stærð viðmótshluta löguð

--- src/main/java/vidmot/plantmania/PlantController.java
+++ src/main/java/vidmot/plantmania/PlantController.java
@@ -1,22 +1,21 @@
 package vidmot.plantmania;
 
 import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 public class PlantController {
     @FXML
     private Plontuyfirlit fxPlonturYfirlit; //mínar plöntur yfirlitið
 
     public void initialize() {
-        
+
     }
 
 
     @FXML
     protected void fxBaetaVidHandler() {
-        Rectangle spjald = new Rectangle(100, 150);
-        spjald.setFill(Color.BLUEVIOLET);
+        Spjald spjald = new Spjald();
+        //Rectangle spjald = new Rectangle(100, 150);
+        //spjald.setFill(Color.BLUEVIOLET);
         fxPlonturYfirlit.getFxFlowPane().getChildren().add(spjald);
     }
 }
