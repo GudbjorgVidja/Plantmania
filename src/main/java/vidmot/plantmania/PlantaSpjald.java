2be8c849baa3aa988f408d9185803b8c7272a664
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-04 22:59:41 +0000

    hreinsaði upp comment og gamlan kóða

--- src/main/java/vidmot/plantmania/PlantaSpjald.java
+++ src/main/java/vidmot/plantmania/PlantaSpjald.java
@@ -1,5 +1,5 @@
 /**
- *
+ * plöntuspjald fyrir einhverja plöntu, hlutur af gerð Planta. það er líka til MinPlantaSpjald
  */
 package vidmot.plantmania;
 
@@ -27,20 +27,7 @@ public class PlantaSpjald extends AnchorPane {
     private Planta planta;//Planta vinnsluhluturinn
 
     public PlantaSpjald() {
-        /*
-        //setja controller og rót
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("planta-view.fxml"));
-        loader.setRoot(this);
-        loader.setController(this);
-        try {
-            loader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-
-         */
-
-
+        
     }
 
     public PlantaSpjald(Planta p) {
@@ -54,30 +41,12 @@ public class PlantaSpjald extends AnchorPane {
             throw new RuntimeException(exception);
         }
 
-        //planta = p;
-
-        //System.out.println("Slodin: " + planta.getMyndaslod());
-        //fxPlontuMynd.setImage(new Image("src/resources/vidmot/plantmania/styling/plants/" + planta.getMyndaslod()));//mynd á spjaldinu rétt?
-
-        //Image image = new Image("styling/plants/" + planta.getMyndaslod(), true);
-        //Image image = new Image("styling/plants/" + planta.getMyndaslod());
-        //Image image = new Image("styling/plants/monstera.png");
-        //Image image = new Image("plants/monstera.png");
-        //Image image = new Image("src/resources/vidmot/plantmania/styling/plants/monstera.png");
-
-        //Image image = new Image(getClass().getResourceAsStream("styling/plants/monstera.png"));//virkar!!!!!!!
-
-        //Image image = new Image("styling/plants/" + planta.getMyndaslod());
-        //System.out.println("image var gert");
-
         fxPlontuMynd.setImage(new Image(getClass().getResourceAsStream("styling/plants/" + planta.getMyndaslod())));//mynd á spjaldinu rétt?
-        //fxPlontuMynd.setImage(image);//mynd á spjaldinu rétt?
 
         fxAlmenntNafn.setText(planta.getAlmenntNafn());
 
         fxFlokkur.setText(" " + planta.getUppruni().toString().toLowerCase(Locale.ROOT) + " ");
-        String s = planta.getUppruni() + "";
-        s = s.toLowerCase(Locale.ROOT);
+        //String s = planta.getUppruni() + "";
+        //s = s.toLowerCase(Locale.ROOT);
     }
-
 }
