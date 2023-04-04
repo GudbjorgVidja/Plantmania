c0cde6b12ed7f322b9244d027900a19b1e5fc7fa
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-04 18:32:09 +0000

    plöntugluggi gerður

--- src/main/java/vidmot/plantmania/Plontugluggi.java
+++ src/main/java/vidmot/plantmania/Plontugluggi.java
@@ -1,6 +1,17 @@
+/**
+ * skoða með að hafa eiginlega accordionPane í skrollanlega partinum. Þyrfti að vera TitledPane hlutir í VBox sem er í
+ * ScrollPane, því accordion leyfir bara eitt opið í einu.
+ * Nota listener til að setja min stærð eftir því hvort titledPane er opið eða lokað
+ */
 package vidmot.plantmania;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Plontugluggi {
+    @FXML
+    private Label fxBreyta;
+
     public static void main(String[] args) {
 
     }
