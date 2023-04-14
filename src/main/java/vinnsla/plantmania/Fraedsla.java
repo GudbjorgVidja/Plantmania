39e9101847ab1c001239695f0d86ff66fe8b6e88
GudbjorgVidja <gvpbiering@gmail.com>
2023-04-14 15:06:08 +0000

    vinnsluklasi gerður til að halda utan um upplýsingarnar í upplýsingaflipanum

---
+++ src/main/java/vinnsla/plantmania/Fraedsla.java
@@ -0,0 +1,9 @@
+package vinnsla.plantmania;
+
+/**
+ * Þessi klasi er ekki góður, og verður tekinn út og í staðinn lesið inn úr skrá. Bara til bráðabirgða.
+ * todo: laga þetta
+ */
+public class Fraedsla {
+
+}
